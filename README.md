8227d332bd02c2114cf65cc4545264eaa1f5c824
FangDingli <1191230628@qq.com>
2022-05-28 20:09:44 +0800

    remove vitest, add auto-import

---
+++ README.md
@@ -0,0 +1,3 @@
+# A Vite start template for myself
+
+A more lite version for [vitesse-lite](https://github.com/antfu/vitesse-lite)
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 import Unocss from 'unocss/vite'
 
 export default defineConfig({
@@ -9,11 +11,37 @@ export default defineConfig({
       '~/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
+  server: {
+    port: 3000,
+    proxy: {
+      "/devServer": {
+        target: "", // here is dev server host
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/devServer/, '')
+      }
+    },
+    fs: {
+      strict: false
+    }
+  },
   plugins: [
     Vue(),
 
     // https://github.com/antfu/unocss
     // see unocss.config.ts for config
     Unocss(),
+
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'vue',
+        '@vueuse/core',
+      ]
+    }),
+
+    // https://github.com/antfu/unplugin-vue-components
+    Components({
+      resolvers: [] // here is third party ui library, see more detail in your library document
+    }),
   ],
 })
